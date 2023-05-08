6ecf26c75a176d81b11237a566ddbd6ccb234896
Carlos Flores <juancarlosff999@gmail.com>
2023-05-08 11:14:31 -0600

    Initial commit

---
+++ app/src/main/java/com/Carlos/proyecto_final/LoginActivity.kt
@@ -0,0 +1,11 @@
+package com.Carlos.proyecto_final
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
